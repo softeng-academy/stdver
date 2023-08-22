7805351714e81020a71aa9a646d69839439dfdad
Ralf S. Engelschall <rse@engelschall.com>
2023-08-22 13:13:07 +0200

    use separated files

---
+++ web/index.js
@@ -0,0 +1,13 @@
+/*
+**  StdVer ~ Standard Versioning
+**  Copyright (c) 2023 SEA Software Engineering Academy gGmbH
+**  Licensed under Creative Commons Attribution 4.0 <https://spdx.org/licenses/CC-BY-4.0.html>
+*/
+
+Vue.createApp({
+    data () {
+        return {
+        }
+    }
+}).mount("body")
+
