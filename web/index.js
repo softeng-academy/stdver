c2ea1f7c2739a27275d6c57453029543b1d0d2af
Ralf S. Engelschall <rse@engelschall.com>
2023-08-22 22:11:55 +0200

    improve website

--- web/index.js
+++ web/index.js
@@ -1,12 +1,27 @@
 /*
 **  StdVer ~ Standard Versioning
 **  Copyright (c) 2023 SEA Software Engineering Academy gGmbH
+**  Copyright (c) 2008-2023 Dr. Ralf S. Engelschall
 **  Licensed under Creative Commons Attribution 4.0 <https://spdx.org/licenses/CC-BY-4.0.html>
 */
 
 Vue.createApp({
     data () {
         return {
+            page: "scheme"
+        }
+    },
+    mounted () {
+        let page = window.location.hash ?? ""
+        page = page.replace(/^#/, "")
+        if (page === "")
+            page = "scheme"
+        this.page = page
+    },
+    methods: {
+        goto (page) {
+            this.page = page
+            window.location.hash = page === "scheme" ? "" : `#${page}`
         }
     }
 }).mount("body")
