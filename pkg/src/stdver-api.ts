8863747bed150d4c79bab4d045f837295c310c2a
Ralf S. Engelschall <rse@engelschall.com>
2023-08-27 22:40:49 +0200

    revamp CLI package

--- pkg/src/stdver-api.ts
+++ pkg/src/stdver-api.ts
@@ -5,21 +5,193 @@
 **  Licensed under MIT license <https://spdx.org/licenses/MIT.html>
 */
 
-export default class StdVer {
-    constructor () {}
-    parse (str: string) {
-        const m = str.match(/^(\d+)\.(\d+)(a|b|rc|\.)(\d+)(?:\.\d{8})?(?:\+[\dA-F]{4})?(?:-(XA|LA|EA|GA))?$/)
+import moment from "moment"
+
+/*  Standard Versioning: Phase  */
+export enum StdVerPhase {
+    alpha      = 1,
+    beta       = 2,
+    candidate  = 3,
+    release    = 4
+}
+
+/*  Standard Versioning: Scope  */
+export enum StdVerScope {
+    XA = 1,
+    LA = 2,
+    EA = 3,
+    GA = 4
+}
+
+/*  Standard Versioning record  */
+export interface StdVer {
+    M: number,
+    N: number,
+    p: StdVerPhase,
+    R: number,
+    D: number,
+    H: string
+    S: StdVerScope
+}
+
+/*  Standard Versioning API  */
+export default class StdVerAPI {
+    /*  decode a Standard Versioning identifier string into a record  */
+    decode (encoding: string): StdVer {
+        const m = encoding.match(/^(\d+)\.(\d+)(a|b|rc|\.)(\d+)(?:\.(\d{8}))?(?:\+([\dA-F]{4}))?(?:-([XLEG]A))?$/)
         if (m === null)
-            throw new Error("invalid version identifier")
-        return {
-            majorVersion:  m[1],
-            minorVersion:  m[2],
-            maturityLevel: m[3],
-            revision:      m[4],
-            date:          m[5],
-            hash:          m[6],
-            scope:         m[7]
+            throw new Error("invalid Standard Versioning identifier")
+        let p: StdVerPhase
+        switch (m[3]) {
+            case "a": p = StdVerPhase.alpha;      break
+            case "b": p = StdVerPhase.beta;       break
+            case "c": p = StdVerPhase.candidate;  break
+            default:  p = StdVerPhase.release;    break
         }
+        let S: StdVerScope
+        switch (m[7]) {
+            case "XA": S = StdVerScope.XA; break
+            case "LA": S = StdVerScope.LA; break
+            case "EA": S = StdVerScope.EA; break
+            default:   S = StdVerScope.GA; break
+        }
+        return {
+            M: parseInt(m[1]),
+            N: parseInt(m[2]),
+            p,
+            R: parseInt(m[4]),
+            D: m[5] ? parseInt(m[5]) : undefined,
+            H: m[6] ?? "",
+            S
+        } as StdVer
+    }
+
+    /*  encode a Standard Versioning record into an identifier  */
+    encode (decoding: StdVer) {
+        let encoding = `${decoding.M}.${decoding.N}`
+        switch (decoding.p) {
+            case StdVerPhase.alpha:     encoding += "a";  break
+            case StdVerPhase.beta:      encoding += "b";  break
+            case StdVerPhase.candidate: encoding += "rc"; break
+            case StdVerPhase.release:   encoding += ".";  break
+        }
+        encoding += `${decoding.R}`
+        if (decoding.D)
+            encoding += `.${decoding.D}`
+        if (decoding.H)
+            encoding += `+${decoding.H}`
+        if (decoding.S) {
+            switch (decoding.S) {
+                case StdVerScope.XA: encoding += "-XA"; break
+                case StdVerScope.LA: encoding += "-LA"; break
+                case StdVerScope.EA: encoding += "-EA"; break
+                case StdVerScope.GA: encoding += "-GA"; break
+            }
+        }
+        return encoding
+    }
+
+    /*  bump a Standard Versioning identifier part  */
+    bump (encoding: string, part: "M"|"N"|"p"|"R"|"D"|"H"|"S" = "R", level = 2) {
+        /*  decode identifier  */
+        let decoding = this.decode(encoding)
+
+        /*  bump identifier part(s)  */
+        if (part === "M") {
+            decoding.M++
+            decoding.N = 0
+            if (level === 0)
+                decoding.p = StdVerPhase.release
+            else
+                decoding.p = StdVerPhase.alpha
+            decoding.R = 0
+            if (decoding.D)
+                decoding.D = parseInt(moment().format("YYYYMMDD"))
+            if (decoding.H)
+                decoding.H = ""
+        }
+        else if (part === "N") {
+            decoding.N++
+            if (level === 0)
+                decoding.p = StdVerPhase.release
+            else
+                decoding.p = StdVerPhase.alpha
+            decoding.R = 0
+            if (decoding.D)
+                decoding.D = parseInt(moment().format("YYYYMMDD"))
+            if (decoding.H)
+                decoding.H = ""
+        }
+        else if (part === "p") {
+            switch (decoding.p) {
+                case StdVerPhase.alpha:     decoding.p = StdVerPhase.beta;      break
+                case StdVerPhase.beta:      decoding.p = StdVerPhase.candidate; break
+                case StdVerPhase.candidate: decoding.p = StdVerPhase.release;   break
+                case StdVerPhase.release:   decoding.p = StdVerPhase.alpha;     break
+            }
+            decoding.R = 0
+            if (decoding.D)
+                decoding.D = parseInt(moment().format("YYYYMMDD"))
+            if (decoding.H)
+                decoding.H = ""
+        }
+        else if (part === "R") {
+            if (decoding.D)
+                decoding.D = parseInt(moment().format("YYYYMMDD"))
+            if (decoding.H)
+                decoding.H = ""
+        }
+        else if (part === "D") {
+            decoding.D = parseInt(moment().format("YYYYMMDD"))
+            if (decoding.H)
+                decoding.H = ""
+        }
+        else if (part === "H") {
+            decoding.H = ""
+        }
+        else if (part === "S") {
+            switch (decoding.S) {
+                case StdVerScope.XA: decoding.S = StdVerScope.LA; break
+                case StdVerScope.LA: decoding.S = StdVerScope.EA; break
+                case StdVerScope.EA: decoding.S = StdVerScope.GA; break
+                case StdVerScope.GA: decoding.S = StdVerScope.XA; break
+            }
+        }
+
+        /*  (re-)encode identifer  */
+        const encodingNew = this.encode(decoding)
+        return encodingNew
+    }
+
+    /*  explain a Standard Versioning identifier  */
+    explain (encoding: string) {
+        const decoding = this.decode(encoding)
+        let text = `Version ${decoding.M}.${decoding.N}'s`
+        if (decoding.R === 0)
+            text += " initial"
+        else {
+            let R = decoding.R + 1
+            text += ` ${R}`
+            if      (R === 1)  text += "st"
+            else if (R === 2)  text += "nd"
+            else if (R === 3)  text += "rd"
+            else               text += "th"
+        }
+        text += " "
+        if      (decoding.p === StdVerPhase.alpha)     text += "alpha release"
+        else if (decoding.p === StdVerPhase.beta)      text += "beta release"
+        else if (decoding.p === StdVerPhase.candidate) text += "release candidate"
+        else                                           text += "release"
+        if (decoding.D) text += `, made on ${decoding.D.toString().replace(/^(....)(..)(..)$/, "$1.$2.$3")}`
+        if (decoding.H) text += `, from source state 0x${decoding.H}`
+        if (decoding.S) {
+            if      (decoding.S === StdVerScope.XA) text += `, intended for no availability`
+            else if (decoding.S === StdVerScope.LA) text += `, intended for limited availability`
+            else if (decoding.S === StdVerScope.EA) text += `, intended for early availability`
+            else if (decoding.S === StdVerScope.GA) text += `, intended for general availability`
+        }
+        text += "."
+        return text
     }
 }
 
--- pkg/src/stdver-cli.ts
+++ pkg/src/stdver-cli.ts
@@ -5,36 +5,79 @@
 **  Licensed under MIT license <https://spdx.org/licenses/MIT.html>
 */
 
-import commander from "commander"
-// import chalk     from "chalk"
+import fs          from "node:fs"
+import { Command } from "commander"
+import chalk       from "chalk"
 
-// @ts-ignore
-import my        from "../package.json"
-// import stdver    from "./stdver-api"
+import StdVerAPI, { StdVerScope, StdVerPhase, StdVer } from "./stdver-api.js"
 
-/*  parse command-line arguments  */
-const program = new commander.Command()
-program.name("stdver")
-    .description("Standard Versioning Command-Line Interface")
-    .showHelpAfterError("hint: use option --help for usage information")
-    .option("-h, --help", "show usage help", false)
-    .option("-V, --version", "show program version information", false)
-    .argument("[<stdver>]", "input file")
-program.parse(process.argv)
-
-/*  handle special help request  */
-if (program.opts().help) {
-    console.log(program.helpInformation())
-    console.log("Example:\n  $ stdver bump 1.2.3 minor\n")
-    process.exit(0)
+class StdVerCLI {
+    private program!: Command
+    main () {
+        /*  parse command-line arguments  */
+        this.program = new Command()
+        this.program.name("stdver")
+            .description("Standard Versioning Command-Line Interface")
+            .showHelpAfterError("hint: use option --help for usage information")
+            .option("-h, --help", "show usage help", false)
+            .action((opts) => { this.help(opts) })
+        this.program.command("help")
+            .action((opts) => { this.help(opts) })
+        this.program.command("version")
+            .action((opts) => { this.version(opts) })
+        this.program.command("bump")
+            .option("-l, --level <level>", "Standard Versioning scheme level", (v: string, p: number) => {
+                const l = parseInt(v, 10)
+                if (isNaN(l))
+                    this.fatal(new Error("invalid option argument: invalid string (expected number)"))
+                if (l < 0 || l > 2)
+                    this.fatal(new Error("invalid option argument: out of range (expected 0..2)"))
+                return l
+            }, 0)
+            .option("-p, --part <part>", "Standard Versioning identifier part to bump", "R")
+            .argument("<version>", "Standard Versioning identifier to change")
+            .action((version, opts) => { try { this.bump(opts, version) } catch (ex) { this.fatal(ex) } })
+        this.program.command("explain")
+            .option("-f, --format <format>", "format of explanation ('text', 'json')", "text")
+            .argument("<version>", "Standard Versioning identifier to explain")
+            .action((version, opts) => { try { this.explain(opts, version) } catch (ex) { this.fatal(ex) } })
+        this.program.parse(process.argv)
+    }
+    fatal (error: any): never {
+        let msg = error instanceof Error ? error.toString() : ("" + error)
+        msg = msg.replace(/^Error:\s+/i, "")
+        process.stderr.write(chalk.red(`stdver: ERROR: ${msg}\n`))
+        process.exit(1)
+    }
+    help (opts: any) {
+        process.stdout.write(this.program.helpInformation() + "\n")
+        process.stdout.write("Examples:\n" +
+            "$ stdver help\n" +
+            "$ stdver version\n" +
+            "$ stdver bump -p N 1.2.3\n" +
+            "$ stdver explain 1.2a3.20230801+ABCD-XA\n\n")
+        process.exit(0)
+    }
+    async version (opts: any) {
+        const pkgFile = new URL("../package.json", import.meta.url)
+        const pkgJSON = await fs.promises.readFile(pkgFile, { encoding: "utf8" })
+        const pkg = JSON.parse(pkgJSON)
+        process.stdout.write(chalk.blue.bold(`${pkg.name} ${pkg.version}`) + ` (node ${process.versions.node})\n`)
+        process.stdout.write(chalk.blue(`${pkg.description}\n`))
+        process.stdout.write(`Copyright (c) 2023 ${pkg.author.name} <${pkg.author.url}>\n`)
+        process.stdout.write(`Licensed under ${pkg.license} <http://spdx.org/licenses/${pkg.license}.html>\n`)
+        process.exit(0)
+    }
+    bump (opts: any, version: string) {
+        const api = new StdVerAPI()
+        const versionNew = api.bump(version, opts.part, opts.level)
+        process.stdout.write(versionNew + "\n")
+    }
+    explain (opts: any, version: string) {
+        const api = new StdVerAPI()
+        const text = api.explain(version)
+        process.stdout.write(text + "\n")
+    }
 }
-
-/*  handle special version request  */
-if (program.opts().version) {
-    console.log(`${my.name} ${my.version} (Node.js ${process.versions.node})`)
-    console.log(`${my.description}`)
-    console.log(`Copyright (c) 2023 ${my.author.name} <${my.author.url}>`)
-    console.log(`Licensed under ${my.license} <http://spdx.org/licenses/${my.license}.html>`)
-    process.exit(0)
-}
+(new StdVerCLI()).main()
 
